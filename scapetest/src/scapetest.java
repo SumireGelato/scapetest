cfecf3cac4652b00b6b2ce3a13555bcc93c0cb55
leo.k.pang@gmail.com <Gwef4mFzaRb9>
2017-05-01 13:23:43 +1000

    Make Test Harness, downloadSeries downloads into seperate files, code organisation

--- scapetest/src/scapetest.java
+++ scapetest/src/scapetest.java
@@ -3,12 +3,13 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.Scanner;
 
 /**
  * Created by Kin To Pang on 15/06/14.
@@ -19,13 +20,60 @@ public class scapetest {
     private static final String host = "http://www.heartofthecards.com";
     private static Document doc;
 
-    public static void main(String[] args)
-            throws IOException {//list format: map<Series name, Series link>>
+    public static void main(String[] args) throws IOException {
 
-        Map<String, String> tdSeries = new HashMap<String, String>();
-        Map<String, String> boosterSeries = new HashMap<String, String>();
-        Map<String, String> extraSeries = new HashMap<String, String>();
-        Map<String, String> miscSeries = new HashMap<String, String>();
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("ZweiSteele V2.0 Test Harness!");
+            System.out.println("1) Download Series");
+            System.out.println("2) Download Series Card List");
+            System.out.println("3) Download All Series Card Lists");
+            System.out.println("4) Download Single Card");
+            System.out.println("5) Download All Cards of a Series");
+            System.out.println("6) Check For Updates");
+            System.out.println("7) Exit");
+            System.out.print("Choice? ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    downloadSeries();
+                    break;
+                case 2:
+                    BufferedReader br = new BufferedReader(new FileReader("trialdecks.txt"));
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                    //downloadSingleCardList("");
+                    break;
+                case 3:
+                    downloadAllCardLists();
+                    break;
+                case 4:
+                    //downloadSingleCard("");
+                    break;
+                case 5:
+                    downloadAllCardsOfASeries();
+                    break;
+                case 6:
+                    checkForUpdate();
+                    break;
+                default:
+                    choice = 7;
+                    break;
+            }
+        } while (choice != 7);
+
+    }
+
+    private static void downloadSeries() throws IOException {
+
+        Map<String, String> tdSeries = new HashMap<>();
+        Map<String, String> boosterSeries = new HashMap<>();
+        Map<String, String> extraSeries = new HashMap<>();
+        Map<String, String> miscSeries = new HashMap<>();
 
         Elements seriesRaw;
         //connect and parse webpage
@@ -72,31 +120,39 @@ public class scapetest {
             }
         }
 
-        PrintWriter writer = new PrintWriter("series.txt", "UTF-8");
-        writer.println("TRIAL DECKS");
+        PrintWriter writer = new PrintWriter("trialdecks.txt", "UTF-8");
         for (HashMap.Entry<String, String> entry : tdSeries.entrySet()) {
             writer.println(entry.getKey() + "|" + entry.getValue());
         }
-        writer.println("BOOSTERS");
+        System.out.println(tdSeries.size() + " Trial Decks Downloaded");
+        writer.close();
+
+        writer = new PrintWriter("boosters.txt", "UTF-8");
         for (HashMap.Entry<String, String> entry : boosterSeries.entrySet()) {
             writer.println(entry.getKey() + "|" + entry.getValue());
         }
-        writer.println("EXTRA PACKS");
+        System.out.println(boosterSeries.size() + " Boosters Downloaded");
+        writer.close();
+
+        writer = new PrintWriter("extra.txt", "UTF-8");
         for (HashMap.Entry<String, String> entry : extraSeries.entrySet()) {
             writer.println(entry.getKey() + "|" + entry.getValue());
         }
-        writer.println("MISC");
+        System.out.println(extraSeries.size() + " Extra Packs Downloaded");
+        writer.close();
+
+        writer = new PrintWriter("misc.txt", "UTF-8");
         for (HashMap.Entry<String, String> entry : miscSeries.entrySet()) {
             writer.println(entry.getKey() + "|" + entry.getValue());
         }
+        System.out.println(miscSeries.size() + " Misc Downloaded");
         writer.close();
     }
 
-/*    //List format map<Card ID,  Array[carddata string,link]>
+    //List format map<Card ID,  Array[carddata string,link]>
     //card data string format: english card name/japanese card name/card type/card color in hex
-    public static TreeMap<String, String[]> getCardList(String link) throws IOException
-    {
-        Map<String, String[]> cardList = new HashMap<String, String[]>();
+    private static void downloadSingleCardList(String link) throws IOException {
+        Map<String, String[]> cardList = new HashMap<>();
 
         Elements cardListRaw;
 
@@ -111,26 +167,26 @@ public class scapetest {
         for (int i = 0; i < cardListRaw.size(); i += 5) {
             //check if the current card is a repeat
             String spCheckerString = cardListRaw.get(i).text().substring(cardListRaw.get(i).text().length() - 3);
-            if (spCheckerString.contains("SP") || spCheckerString.contains("R") || spCheckerString.contains("S")) {
-                //Skip
-            } else {
-                StringBuilder str = new StringBuilder();
-                str.append(StringEscapeUtils.unescapeXml(cardListRaw.get(i + 1).text() + "/"));//ID
-                str.append(StringEscapeUtils.unescapeXml(cardListRaw.get(i + 2).childNode(0).toString() + "/"));//Eng Name
-                str.append(StringEscapeUtils.unescapeXml(cardListRaw.get(i + 2).childNode(2).toString() + "/"));//Jp Name
-                str.append(cardListRaw.get(i + 3).text() + "/");//Type
-                str.append(cardListRaw.get(i + 4).attr("bgcolor"));//Color
-                String[] cardDetails = {str.toString(), cardListRaw.get(i + 1).attr("href")};
+            if (!spCheckerString.contains("SP") && !spCheckerString.contains("R") && !spCheckerString.contains("S")) {
+                String str = (cardListRaw.get(i + 3).text() + "/") +//Type
+                        cardListRaw.get(i + 4).attr("bgcolor");
+//                str.append(StringEscapeUtils.unescapeXml(cardListRaw.get(i + 1).text() + "/"));//ID
+//                str.append(StringEscapeUtils.unescapeXml(cardListRaw.get(i + 2).childNode(0).toString() + "/"));//Eng Name
+//                str.append(StringEscapeUtils.unescapeXml(cardListRaw.get(i + 2).childNode(2).toString() + "/"));//Jp Name
+                String[] cardDetails = {str//Color
+                        , cardListRaw.get(i + 1).attr("href")};
                 cardList.put(cardListRaw.get(i).text(), cardDetails);
             }
+            //Skip
         }
-        TreeMap<String, String[]> cardListSorted = new TreeMap<String, String[]>(cardList);
-        return cardListSorted;
     }
 
-    public static HashMap<String, String> getSingleCard(String link)
-            throws IOException {
-        HashMap<String, String> cardData = new HashMap<String, String>();
+    private static void downloadAllCardLists() {
+
+    }
+
+    private static void downloadSingleCard(String link) throws IOException {
+        HashMap<String, String> cardData = new HashMap<>();
         Elements singleCardHeadings;
         Elements singleCardData;
 
@@ -151,6 +207,13 @@ public class scapetest {
                 cardData.put(singleCardHeadings.get(i - 2).text().split(":")[0], singleCardData.get(i).text());
             }
         }
-        return cardData;
-    }*/
+    }
+
+    private static void downloadAllCardsOfASeries() {
+
+    }
+
+    private static void checkForUpdate() {
+
+    }
 }
